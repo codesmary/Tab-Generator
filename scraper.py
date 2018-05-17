85a1530eef9119afcc66c05e2faba8688e5fa650
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2018-05-16 19:31:23 -0500

    initial commit

--- scraper.py
+++ scraper.py
@@ -12,11 +12,12 @@ def main(argv=sys.argv):
     try:
         os.makedirs("songs")
     except:
+        #remove all the files in the songs directory
+        #is this line necessary
         pass
+
     browser = webdriver.Firefox()
     browser.get("https://www.ultimate-guitar.com/explore")
-
-    #deal with possibility of artist not existing
 
     for artist in artists:
         search = browser.find_element_by_tag_name("input")
@@ -25,30 +26,25 @@ def main(argv=sys.argv):
         
         time.sleep(5)
 
+        #if artist doesn't exist, continue
+
+        #create a set of songs for this artist
         num_tabs = browser.find_element_by_class_name("_2PyWj")
         num_tabs = int(num_tabs.text.split()[0])
-
-        #click forward to pages [2,n], incrementing by 1 page
-        #for each number of divisible elements of 50 in the
-        #total number of tabs
-        print(artist + " " + str(num_tabs) + " " + str(math.ceil(num_tabs/50)))
         for page in range(1,math.ceil(num_tabs/50)+1):
-            if page != 1:
+            if page > 1:
+                num_tabs -= 50
                 next = browser.find_element_by_partial_link_text("page=" + str(page))
                 next.click()
-        #set of found songs for the artist
-        #search their name in the search bar
-            #for each of their songs
-                #if the song title is not already in found songs and 
-                #if the song title has parenthesis at the end, cut those 
-                #off and check to see that the original song title isn't 
-                #already in found songs
+                time.sleep(5)
+            for tab in range(math.min(num_tabs,50)):
+                #if the song text before optional parenthesis
+                #doesn't match a song in the song set, 
                     #click on the link
+                    #create a file
                     #write the lyrics to a file
-                    #store the file in the created song directory
-                    #click the back button
-        #elem.back()
-        time.sleep(5)
+                    #store the file in the songs directory
+                    #click on the back button
     
     browser.close()
 
