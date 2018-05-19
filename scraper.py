624e701e474ce899d2a54ea7a1f78fc2803a2e48
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2018-05-19 14:37:30 -0500

    writes raw tabs to files

--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 
 def main(argv=sys.argv):
     #replace with getting input from the command line
-    artists = ["Julia Nunes", "Conan Gray", "Remo Drive"]
+    artists = ["Conan Gray", "Julia Nunes", "Remo Drive"]
 
     try:
         os.makedirs("songs")
@@ -30,7 +30,7 @@ def main(argv=sys.argv):
         except:
             continue
        
-        songs = {}
+        songs = set()
         num_tabs = int(num_tabs.text.split()[0])
         for page in range(1,math.ceil(num_tabs/50)+1):
             if page > 1:
@@ -40,6 +40,7 @@ def main(argv=sys.argv):
                 time.sleep(5)
             tabs = browser.find_elements_by_class_name("_1iQi2")
             for tab in range(1,len(tabs)):
+                tabs = browser.find_elements_by_class_name("_1iQi2")
                 title = tabs[tab].text
                 #remove clutter before song title (4 variations)
                 #1. the artist name + "\n"
@@ -52,14 +53,29 @@ def main(argv=sys.argv):
                 #2. * and all following text (* indicates a simplified tab)
                 #3. "\n" and all following text (song title will be contained in the first line)
                 title = remove(title,r'(\s*[(\*\n]+[\s\S]*)|(\n+[\s\S]*)')
-                print(title)
-                #if the song text doesn't match a song in the song set
+                if title not in songs:
+                    songs.add(title)
                     #click on the link
+                    song_link = browser.find_element_by_partial_link_text(title)
+                    song_link.click()
+                    time.sleep(5)
                     #create a file
+                    song_name = title.replace(" ","_") + ".txt"
+                    file = open(os.path.join("songs",song_name),"w")
                     #write the lyrics to a file
-                    #store the file in the songs directory
-                    #click on the back button
-    
+                    lyrics = browser.find_element_by_class_name("_1YgOS")
+                    lyrics = lyrics.text
+                    file.write(lyrics)
+                    file.close()
+                    
+                    try:
+                        close_popup = browser.find_element_by_link_text("No, thanks. I'm not looking for easy way")
+                        close_popup.close()
+                    except:
+                        pass
+                    
+                    browser.back()
+                    time.sleep(5)
     browser.close()
 
 def remove(song,filler):
