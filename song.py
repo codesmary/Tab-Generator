905e9d2670c7506dc4ae6c93ac343215033c18b6
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2018-05-24 11:59:16 -0500

    first working word capturing

--- song.py
+++ song.py
@@ -11,49 +11,54 @@ class Song:
 
     def generate_tab(self, tab):
         lyrics = []
-        line = re.compile(r"^([ ]*([a-gA-GIiJjMmNnSsUu./\\\-#*1-9\[\]()][ ]*)+)\n([A-zÀ-ú0-9,:;''’\".()\-\?! ]+)\n?$", re.MULTILINE)
+        line = re.compile(r"^([ ]*([a-gA-GIiJjMmNnSsUu./\\\-#*1-9\[\]()][ ]*)+)\n([A-zÀ-ú0-9,:;''’\".()\-\?! ]+\n{0,2})", re.MULTILINE)
         line_matches = line.finditer(tab)
         match_len = 0
         for match in line_matches:
             chords = match.group(1)
-            words = match.group(3) + ' '
+            words = match.group(3)
             chord_ptr = 0
             word_ptr = 0
             for char in range(min(len(chords),len(words))):
-                if words[char] == ' ':
-                    chord = chords[chord_ptr:char+1].strip()
-                    word = words[word_ptr:char+1].strip()
+                if words[char] == ' ' or words[char] == '\n':
+                    end = char+1
+                    chord = chords[chord_ptr:end]
+                    end -= 1
+                    while end < len(chords) - 1 and chords[end] != ' ' and chords[end] != '\n':
+                        end += 1
+                        chord += chords[end]
+                    chord = chord.strip()
+                    word = words[word_ptr:char+1].replace(' ','')
                     if len(chord) == 0:
                         chord = None
                     if len(word) == 0:
                         word = None
-                    else:
+                    elif word[-1] != '\n':
                         word += ' '
-                    chord_ptr = char + 1
+                    chord_ptr = end + 1
                     word_ptr = char + 1
                     if chord != None or word != None:
-                        #print('chord ' + ('none' if chord==None else chord))
-                        #print('word ' + ('none' if word==None else word))
                         lyrics.append(Word(chord,word))
             if len(chords) > len(words):
-                lyrics[-1].word += '\n'
+                lyrics[-1].word = lyrics[-1].word[:-1]
                 remaining = chords[chord_ptr:].split()
                 for chord in remaining:
-                    #print('chord ' + chord)
-                    #print('no word')
                     lyrics.append(Word(chord,None))
             elif len(words) > len(chords):
                 remaining = words[word_ptr:].split()
+                if words[-1] == '\n':
+                    remaining[-1] += '\n'
+                if words[-2] == '\n':
+                    remaining[-1] += '\n'
                 for word in range(len(remaining)):
                     rem_chords = chords[chord_ptr:].strip()
                     if word == 0 and len(rem_chords) > 0:
                         chord = rem_chords
                     else:
                         chord = None
-                    #print('chord ' + ('none' if chord==None else chord))
-                    #print('word ' + remaining[word])
-                    lyrics.append(Word(chord,remaining[word] + ' '))
-                lyrics[-1].word = lyrics[-1].word[:-1] + '\n'
+                    if remaining[word][-1] != '\n':
+                        remaining[word] += ' '
+                    lyrics.append(Word(chord,remaining[word]))
             else:
                 lyrics[-1].word += '\n'
             match_len += len(match.group())
@@ -62,5 +67,10 @@ class Song:
             return None
         else:
             for lyric in lyrics:
-                print(lyric.word,end="")
+                if lyric.word != None:
+                    print(('(' + lyric.chord + ')' if lyric.chord != None else "") + lyric.word,end="")
+                else:
+                    #for tab generation, None will be represented
+                    #as \t\n
+                    print(' (' + lyric.chord + ')\t\n')
             return lyrics
