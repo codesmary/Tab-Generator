1cb7c1e60f1c132e80ac6a0434b3045c4975db4c
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2018-05-24 15:59:39 -0500

    added multiple types of no chord spacers, checks for nc

--- song.py
+++ song.py
@@ -1,7 +1,6 @@
 from word import Word
 import re
 
-#need to continue testing on other songs
 class Song:
     def __init__(self, raw_tab):
         lyrics = self.generate_tab(raw_tab)
@@ -19,8 +18,11 @@ class Song:
             words = match.group(3)
             chord_ptr = 0
             word_ptr = 0
-            for char in range(min(len(chords),len(words))):
-                if words[char] == ' ' or words[char] == '\n':
+            #print(str(len(chords)) + ' ' + str(len(words)))
+            #print(chords)
+            #print(words)
+            for char in range(min(len(chords),len(words))-1):
+                if words[char+1] == ' ' or words[char+1] == '\n':
                     end = char+1
                     chord = chords[chord_ptr:end]
                     end -= 1
@@ -29,7 +31,7 @@ class Song:
                         chord += chords[end]
                     chord = chord.strip()
                     word = words[word_ptr:char+1].replace(' ','')
-                    if len(chord) == 0:
+                    if len(chord) == 0 or ('N' in chord.upper() and 'C' in chord.upper()):
                         chord = None
                     if len(word) == 0:
                         word = None
@@ -38,29 +40,49 @@ class Song:
                     chord_ptr = end + 1
                     word_ptr = char + 1
                     if chord != None or word != None:
-                        lyrics.append(Word(chord,word))
+                        if word == None:
+                            lyrics.append(Word(chord,word,'beg'))
+                        else:
+                            lyrics.append(Word(chord,word))
             if len(chords) > len(words):
-                lyrics[-1].word = lyrics[-1].word[:-1]
                 remaining = chords[chord_ptr:].split()
-                for chord in remaining:
-                    lyrics.append(Word(chord,None))
+                if len(remaining) > 0:
+                    #remove trailing white space
+                    lyrics[-1].word = lyrics[-1].word[:-1]
+                    for chord in remaining:
+                        if 'N' not in chord.upper() or 'C' not in chord.upper():
+                            lyrics.append(Word(chord,None,'mid'))
+                    lyrics[-1].pos = 'end'
+                else:
+                    lyrics[-1].word = lyrics[-1].word[:-1] + '\n'
             elif len(words) > len(chords):
                 remaining = words[word_ptr:].split()
-                if words[-1] == '\n':
+                if len(remaining) > 0 and words[-1] == '\n':
                     remaining[-1] += '\n'
-                if words[-2] == '\n':
+                if len(remaining) > 0 and words[-2] == '\n':
                     remaining[-1] += '\n'
                 for word in range(len(remaining)):
                     rem_chords = chords[chord_ptr:].strip()
                     if word == 0 and len(rem_chords) > 0:
                         chord = rem_chords
+                        if 'N' in chord.upper() and 'C' in chord.upper():
+                            chord = None
                     else:
                         chord = None
                     if remaining[word][-1] != '\n':
                         remaining[word] += ' '
                     lyrics.append(Word(chord,remaining[word]))
+                chord = chords[chord_ptr:].strip()
+                if len(remaining) == 0 and len(chord) > 0 and not ('N' in chord.upper() and 'C' in chord.upper()):
+                    lyrics[-1].word = lyrics[-1].word[:-1]
+                    lyrics.append(Word(chord,None,end))
             else:
                 lyrics[-1].word += '\n'
+                if chords[-1] != ' ':
+                    ptr = -1
+                    while chords[ptr] != ' ':
+                        ptr -= 1
+                    lyrics[-1].chord = chords[ptr:].strip()
             match_len += len(match.group())
         #if a small percentage of the tab was correct matches, then return error
         if match_len / len(tab) < 0.5:
@@ -71,7 +93,11 @@ class Song:
                 if lyric.word != None:
                     print(('(' + lyric.chord + ')' if lyric.chord != None else "") + lyric.word,end="")
                 else:
-                    #for tab generation, None will be represented
-                    #as \t\n
-                    print(' (' + lyric.chord + ')\t\n')
+                    #how None's will be interpreted in tab creation
+                    if lyric.pos == 'beg':
+                        print('(' + lyric.chord + ') ',end='')
+                    elif lyric.pos == 'mid':
+                        print('(' + lyric.chord + ')',end='')
+                    else:
+                        print(' (' + lyric.chord + ')\t\n',end='')
             return lyrics
--- word.py
+++ word.py
@@ -1,4 +1,6 @@
 class Word:
-    def __init__(self, chord, word):
+    #the chord string can possibly contain more than 1 chord
+    def __init__(self, chord, word, pos = "na"):
         self.chord = chord
         self.word = word
+        self.pos = pos
